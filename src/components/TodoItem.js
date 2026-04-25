6f19c5e9eedabf197b0999bbbfe56c398b247fea
aiera2876 <aiera2876@gmail.com>
2026-04-25 12:28:36 +0545

    CSS replace

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,8 +3,7 @@ import React from "react";
 function TodoItem({ todo, toggleTodo, deleteTodo }) {
   return (
     <div className="todo-item">
-      <span
-      className="todo-text"
+      <span className="todo-text"
         onClick={() => toggleTodo(todo.id)}
         style={{
           textDecoration: todo.completed ? "line-through" : "none",
