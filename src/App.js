e32bba1c01706ebaa8b4fd2878cc82525732d763
aiera2876 <aiera2876@gmail.com>
2026-04-25 12:14:48 +0545

    Added Todo App UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>To-Do App</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        toggleTodo={toggleTodo}
+        deleteTodo={deleteTodo}
+      />
     </div>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function TodoItem({ todo, toggleTodo, deleteTodo }) {
+  return (
+    <div className="todo-item">
+      <span
+      className="todo-text"
+        onClick={() => toggleTodo(todo.id)}
+        style={{
+          textDecoration: todo.completed ? "line-through" : "none",
+          cursor: "pointer",
+        }}
+      >
+        {todo.text}
+      </span>
+
+      <button onClick={() => deleteTodo(todo.id)}>❌</button>
+    </div>
+  );
+}
+
+export default TodoItem;
